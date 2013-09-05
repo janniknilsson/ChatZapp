4996f643b47712a575a69b5e6ab680afaec2aed3
alinulms <alinulms@gmail.com>
2013-09-05 19:23:41 +0200

    added gray background

--- SignalR/Infrastructure/MongoDataProvider.cs
+++ SignalR/Infrastructure/MongoDataProvider.cs
@@ -7,7 +7,6 @@ namespace SignalR.Infrastructure
   {
     public static MongoDatabase Get()
     {
-      //const string connectionString = "mongodb://localhost";
       const string connectionString = "mongodb://alin:alin@paulo.mongohq.com:10041/Solr";
       MongoClient client = new MongoClient(connectionString);
       var server = client.GetServer();
